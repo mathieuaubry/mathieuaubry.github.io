c2c28a70ae9842d80b06e514729efa4e89bf910a
mathieuaubry <mathieu.aubry@imagine.enpc.fr>
2024-04-13 19:50:08 +0200

    updated from script

--- update_to_github.sh
+++ update_to_github.sh
@@ -2,4 +2,4 @@ git add .
 git commit -m 'updated from script'
 git push
 git pull
-rsync -avr --progress --exclude='projects' * imagine2:~/www/
+rsync -avr --progress --exclude='projects' * imagine4:~/www/
